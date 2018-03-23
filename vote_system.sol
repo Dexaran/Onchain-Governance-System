85c3a870a498b7825d29ae10410975874b5794a9
Dexaran <dexaran820@gmail.com>
2018-03-23 13:45:05 +0000

    Proposal structures implementation

--- vote_system.sol
+++ vote_system.sol
@@ -4,23 +4,45 @@ contract VoteSystem {
     
     uint256 public vote_duration = 345600; // 5760 blocks/day >> 5760 * 30 * 2 = 345600 blocks/2months.
     uint256 public last_vote_index = 0;
+    uint256 public voting_threshold = 10e19; // 10 ETC.
     
-    mapping (uint256 => result[]) public votes;
-    mapping (address => uint256)  public balances;
+    mapping (uint256 => proposal) public vote_proposals;
+    
+    mapping (address => voter)  public voters;
     
     struct result
     {
         string option_name;
-        string description;
-        bytes  data;
+        bool transaction; // true = execute a transaction, false = cast an event
+        
+        address to;
+        bytes   data;
+        //uint256 value;    // always 0 for ETC votes.
+    }
+    
+    struct proposal
+    {
+        result[] results;
+        string   description;
+        uint256  timestamp;
+    }
+    
+    struct voter
+    {
+        uint256 balance;
+        uint256 timestamp;
     }
     
     function submit_vote() payable
     {
+        require(msg.value >= voting_threshold);
         
     }
     
-    
+    function cast_vote(uint256 _voting_index, uint256 _voting_result) payable
+    {
+        
+    }
     
     
     // DEBUGGING FUNCTIONALITY.
